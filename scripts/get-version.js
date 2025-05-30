23abb65a413423404735e66a0dd66c28a347b2e7
Ditmar <ditmarcastro@gmail.com>
2025-05-30 15:41:14 -0400

    feat: test version code

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -3,4 +3,4 @@ const path = require('path');
 const packageJsonPath = path.join(__dirname, '../package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 const version = packageJson.version;
-console.log(version);
+console.log(`VERSION=${version}`);
