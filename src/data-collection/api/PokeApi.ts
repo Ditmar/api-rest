32f795bc792602c2d962627be43273c730e53768
DianaMendoza-11 <rousdidi15@gmail.com>
2025-06-06 23:14:13 -0400

    feature(common): crud usuarios [BACKEND-5]

--- src/data-collection/api/PokeApi.ts
+++ src/data-collection/api/PokeApi.ts
@@ -10,16 +10,15 @@ class PokeApi extends BaseCollection {
         const json = await data.json();
         return json;
     }
-    post(body: unknown): Promise<unknown> {
+    post(): Promise<unknown> {
         throw new Error("Method not implemented.");
     }
-    delete(body: unknown): Promise<unknown> {
+    delete(): Promise<unknown> {
         throw new Error("Method not implemented.");
     }
-    put(body: unknown): Promise<unknown> {
+    put(): Promise<unknown> {
         throw new Error("Method not implemented.");
     }
-    
 }
 
 export { PokeApi };
