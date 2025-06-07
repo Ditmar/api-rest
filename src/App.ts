2b8f0f303773ea3eb7a46e90d7db4d08414b045f
DianaMendoza-11 <rousdidi15@gmail.com>
2025-06-06 22:48:47 -0400

    feature(common): crud usuarios [BACKEND-5]

--- src/App.ts
+++ src/App.ts
@@ -3,6 +3,8 @@ import { ConfigSingleton } from './config/config';
 import { userWrapper } from './user/userRoutes';
 import { DataCollectionFactory } from './data-collection/factory';
 import { BaseCollection } from './data-collection/base-collection/baseCollection';
+import { connectMongoDB } from './db/mongo';
+
 console.log('Development mode');
 
 
@@ -12,12 +14,19 @@ class App {
     private dataCollection: BaseCollection | null = null;
     constructor() {
         this.initializeMiddlewares();
+        this.initDatabase();
         this.initCollections();
         this.initializeRoutes();
     }
 
+    private async initDatabase() {
+        await connectMongoDB()
+    }
+
     private initCollections() {
-       this.dataCollection =  DataCollectionFactory.createDataCollection('api');
+    //    this.dataCollection =  DataCollectionFactory.createDataCollection('api');
+    this.dataCollection =  DataCollectionFactory.createDataCollection('mongo');
+
     }
     private initializeMiddlewares() {
         server.use(express.json());
@@ -28,7 +37,7 @@ class App {
         if (!this.dataCollection) {
             throw new Error('Data collection is not initialized');
         }
-        server.use('/user', userWrapper(this.dataCollection));
+        server.use('/usuarios', userWrapper(this.dataCollection));
     }
 }
 new App();
--- src/data-collection/base-collection/baseCollection.ts
+++ src/data-collection/base-collection/baseCollection.ts
@@ -1,5 +1,5 @@
 export abstract class BaseCollection {
-    abstract get(): Promise<unknown>;
+    abstract get(id?:string): Promise<unknown>;
     abstract post(body: unknown): Promise<unknown>;
     abstract delete(body: unknown): Promise<unknown>;
     abstract put(body: any): Promise<unknown>;
