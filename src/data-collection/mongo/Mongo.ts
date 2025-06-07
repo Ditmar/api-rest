16803fc1e74c81e19700ba8bb9ce04e44668f661
DianaMendoza-11 <rousdidi15@gmail.com>
2025-06-06 22:48:47 -0400

    feature(common): crud usuarios [BACKEND-5]

--- src/data-collection/mongo/Mongo.ts
+++ src/data-collection/mongo/Mongo.ts
@@ -1,19 +1,70 @@
 import { BaseCollection } from "../base-collection/baseCollection";
+import { HttpStatus } from "../../utils/httpStatus";
+import { ErrorHandler } from "../../utils/errorHandler";
+import { UserModel } from '../../models/user.model'
 
 class Mongo extends BaseCollection {
-    get(): Promise<unknown> {
-        return new Promise((resolve) => {
-            resolve([{data: "MongoDB"}, {data: "MongoDB"}])
-        });
+    // get(): Promise<unknown> {
+    //     return new Promise((resolve) => {
+    //         resolve([{data: "MongoDB"}, {data: "MongoDB"}])
+    //     });
+    // }
+
+    async get(id?:string):Promise<unknown>{
+        try {
+            if(id){
+                if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+                    throw new ErrorHandler(HttpStatus.BAD_REQUEST, 'Invalid user ID format');
+                }
+                const user = await UserModel.findById(id).select('-password');
+                if(!user) {
+                    throw new ErrorHandler(HttpStatus.NOT_FOUND, 'User not found');
+                }
+                return user;
+            }
+            return await UserModel.find().select('-password');
+        } catch (error) {
+            throw error;
+        }
     }
-    post(body: unknown): Promise<unknown> {
-        throw new Error("Method not implemented.");
+    
+    async post(body: unknown): Promise<unknown> {
+        const { nombre, email, password } = body as { nombre: string; email: string; password: string };
+        const existingUser = await UserModel.findOne({email});
+        if (existingUser) {
+            throw new ErrorHandler(HttpStatus.BAD_REQUEST, 'User already exists');
+        }
+        const newUser = new UserModel({nombre, email, password});
+        await newUser.save();
+        return newUser;
     }
-    delete(body: unknown): Promise<unknown> {
-        throw new Error("Method not implemented.");
+
+    async delete(id: string): Promise<unknown> {
+        const deletedUser = await UserModel.findByIdAndDelete(id);
+        if (!deletedUser) {
+            throw new ErrorHandler(HttpStatus.NOT_FOUND, 'User not found');
+        }
+        return { message:'User deleted successfully' };
     }
-    put(body: unknown): Promise<unknown> {
-        throw new Error("Method not implemented.");
+    async put(id:string, body: unknown): Promise<unknown> {
+        try {
+            const { nombre, email, password } = body as { nombre: string; email: string; password: string };
+            const user = await UserModel.findById(id);
+            if (!user) {
+                throw new ErrorHandler(HttpStatus.NOT_FOUND, 'User not found');
+            }
+
+            if(email && email !== user.email) {
+                const existingUser = await UserModel.findOne({ email });
+                if (existingUser) {
+                    throw new ErrorHandler(HttpStatus.BAD_REQUEST, 'Email already in use');
+                }
+            }
+
+            return UserModel.findByIdAndUpdate(id, { nombre, email, password }, { new: true }).select('-password');
+        } catch (error) {
+            
+        }
     }
 }
 export { Mongo }
