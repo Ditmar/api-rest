c52264e528d21614224b63acb8639d92218c37b4
Carlos G.Calancha V. <giovannivalverde@hotmail.com>
2025-06-25 08:23:39 -0400

    feat: add deploy years endpoints

--- src/App.ts
+++ src/App.ts
@@ -4,7 +4,8 @@ import { userWrapper } from './user/userRoutes';
 import { DataCollectionFactory } from './data-collection/factory';
 import { BaseCollection } from './data-collection/base-collection/baseCollection';
 import { MongoClient as MongoConnection } from './data-collection/mongo/mongo-client'; 
-
+import { YearCollection } from './year/year.model';
+import { yearWrapper } from './year/yearRoutes';
 
 console.log('Development mode');
 
@@ -33,6 +34,7 @@ class App {
       throw new Error('Data collection is not initialized');
     }
     server.use('/user', userWrapper(this.dataCollection));
+    server.use('/year', yearWrapper(new YearCollection()));
   }
 }
 
---
+++ src/year/year.model.ts
@@ -0,0 +1,45 @@
+import { BaseCollection } from '../data-collection/base-collection/baseCollection';
+
+export interface Year {
+  year: number;
+}
+
+class YearCollection extends BaseCollection {
+  get(): Promise<Year[]> {
+    return this.getAllYears();
+  }
+
+  async getAllYears(): Promise<Year[]> {
+    return [
+      { year: 2020 },
+      { year: 2021 },
+      { year: 2022 },
+      { year: 2023 }
+    ];
+  }
+
+  async createYear(year: Year): Promise<Year> {
+    if (!year || typeof year.year !== 'number' || year.year.toString().length !== 4) {
+      throw new Error('Invalid year format');
+    }
+    
+    return year;
+  }
+
+  async deleteYear(year: number): Promise<boolean> {
+    const hasArticles = await this.checkIfYearHasArticles(year);
+    
+    if (hasArticles) {
+      throw new Error(`Cannot delete year ${year} because it has associated articles`);
+    }
+    
+    return true;
+  }
+
+  private async checkIfYearHasArticles(year: number): Promise<boolean> {
+    const yearsWithArticles = [2021, 2023];
+    return yearsWithArticles.includes(year);
+  }
+}
+
+export { YearCollection, Year };
