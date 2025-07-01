ce257da1e3461509dc70668680073d8eba5924ad
JechJosia5 <josiasjechc0@gmail.com>
2025-07-01 12:11:29 -0400

    UPDATE 2

---
+++ src/App.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import { ConfigSingleton } from './config/config';
+import { userWrapper } from './user/userRoutes';
+import { indexesWrapper } from './Indexes/routes';
+import { DataCollectionFactory } from './data-collection/factory';
+import { BaseCollection } from './data-collection/base-collection/baseCollection';
+import { MongoClient as MongoConnection } from './data-collection/mongo/mongo-client'; 
+
+
+console.log('Development mode');
+
+const server = express();
+
+class App {
+  private dataCollection: BaseCollection | null = null;
+
+  constructor() {
+    this.initializeMiddlewares();
+    this.initCollections();
+    this.initializeRoutes();
+  }
+
+  private initCollections() {
+    this.dataCollection = DataCollectionFactory.createDataCollection('mongo'); 
+    this.dataCollection = DataCollectionFactory.createDataCollection('indexes');
+  }
+
+  private initializeMiddlewares() {
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+  }
+
+  private initializeRoutes() {
+    if (!this.dataCollection) {
+      throw new Error('Data collection is not initialized');
+    }
+    server.use('/user', userWrapper(this.dataCollection));
+    server.use('/indexes', indexesWrapper(this.dataCollection));
+  }
+}
+
+async function startServer() {
+  try {
+   
+    MongoConnection.getInstance();
+   
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    new App();
+    server.listen(ConfigSingleton.getInstance().PORT, () => {
+      console.log(`Server is running on port ${ConfigSingleton.getInstance().PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
+}
+
+startServer();
